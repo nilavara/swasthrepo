9dafd100b8407b0b72355ac964fa16bd3954a52b
rajithrp <rajith.rp@gmail.com>
2017-09-23 12:04:23 -0400

    Initial code
    reference code base

---
+++ swasth-reference-app/src/main/java/com/swasth/matrix/Application.java
@@ -0,0 +1,13 @@
+package com.swasth.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
